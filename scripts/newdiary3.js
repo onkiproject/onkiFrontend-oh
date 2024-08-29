46779780022a68386f0beffacc1cfe03463b91f6
5playing <127284147+5playing@users.noreply.github.com>
2024-08-30 00:06:25 +0900

    add alert
    비밀번호 !== 비밀번호 확인
    일 때 경고창 추가했어요

--- scripts/newdiary3.js
+++ scripts/newdiary3.js
@@ -4,9 +4,13 @@ const input2 = document.querySelector('.diarypasswordinput2');
   
 function handler(){
     if (input.value.trim() === "" || input2.value.trim() === ""){
-    alert("비밀번호를 모두 입력해주세요!");
-    }else{
-    location.href='newdiary4.html';
-   }
+        alert("비밀번호를 모두 입력해주세요!");
+    }
+    else if (input.value.trim() !== input2.value.trim()){
+        alert("비밀번호와 비밀번호 확인이 같지 않아요!");
+    }
+    else{
+        location.href='newdiary4.html';
+    }
 }
 button.addEventListener('click', handler);     
